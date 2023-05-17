c754cc49f4e4bc525b8c036127c901839d0a3554
Jvlio Caio <juliocaionouv@gmail.com>
2023-05-17 02:54:58 -0300

    docker commands to stop and rm container

--- start.services.sh
+++ start.services.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+docker stop bot
+docker rm bot
 docker build -t bot-shopify .
 docker run --name bot -p 4444:4444 -p 5900:5900 --shm-size=2g --restart=always -d -it bot-shopify
 sleep 5
 # docker exec -it bot bash -c "while true; do /usr/bin/python3 /app/shop.py; sleep 3600; done" 
-docker exec -d bot bash -c "while true; do /usr/bin/python3 /app/shop.py >> /app/shop.log 2>&1; sleep 3600; done"
+# docker exec -d bot bash -c "while true; do /usr/bin/python3 /app/shop.py >> /app/shop.log 2>&1; sleep 3600; done"
